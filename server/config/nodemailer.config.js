511efcf799dace764cfa961ccbbf0c2e5ceaf26a
IamDevTrivedi <devtrivedi22082005@gmail.com>
2026-01-11 18:50:16 +0530

    migrate: nodemailer to RESTapi email service

--- server/config/nodemailer.config.js
+++ server/config/nodemailer.config.js
@@ -1,23 +1,52 @@
-const nodemailer = require("nodemailer");
-const logger = require("../utils/logger.utils.js");
-const sgTransport = require("nodemailer-sendgrid-transport");
-
+const axios = require("axios");
 const dotenv = require("dotenv");
 dotenv.config();
+const logger = require("../utils/logger.utils");
 
-const transporter = nodemailer.createTransport(
-  sgTransport({
-    auth: {
-      api_key: process.env.SENDGRID_API_KEY,
-    },
-  })
-);
+const brevoClient = axios.create({
+  baseURL: "https://api.brevo.com/v3",
+  headers: {
+    "Content-Type": "application/json",
+    "api-key": process.env.BREVO_API_KEY,
+  },
+});
+
+const transporter = {
+  sendMail: async ({ from, to, subject, html }) => {
+    try {
+      const emailData = {
+        sender: {
+          email: from,
+        },
+        to: [{
+          email: to,
+        }],
+        subject: subject,
+        htmlContent: html,
+      };
+
+      await brevoClient.post("/smtp/email", emailData);
+    } catch (error) {
+      logger.error("Failed to send email");
+      logger.error(error);
+    }
+  },
+
+  verify: async (callback) => {
+    try {
+      await brevoClient.get("/account");
+      if (callback) callback(null, true);
+    } catch (error) {
+      if (callback) callback(error, false);
+    }
+  },
+};
 
 transporter.verify((error, success) => {
-  if (error) {
-    logger.error("Nodemailer configuration error:", error);
+  if (!error) {
+    logger.info("Nodemailer is ready to send emails");
   } else {
-    logger.info("✅ Nodemailer is ready to send emails");
+    logger.error("Nodemailer configuration error:", error);
   }
 });
 
